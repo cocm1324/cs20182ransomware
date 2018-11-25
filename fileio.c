c1231e3afd831ffbf57fa9822a3137d034967fa1
cocm1324 <cocm1324@gmail.com>
2018-11-25 19:05:44 +0900

    fileio
    dasf

--- fileio.c
+++ fileio.c
@@ -1,11 +1,71 @@
 #include "fileio.h"
 #include "util.h"
 
+int filesize(char* filename){
+    FILE* fileptr = NULL;
+    int filelen;
+
+    fileptr = fopen(filename, "rb");                   // Open the file in binary mode
+    fseek(fileptr, 0, SEEK_END);                       // Jump to the end of the file
+    filelen = ftell(fileptr);                          // Get the current byte offset in the file
+    rewind(fileptr);
+
+    fclose(fileptr); // Close the file
+    fileptr = NULL;
+
+    return filelen + 1;
+}
+
 char* encrypt_file_byte_read(char* filename){
-    FILE* fileptr;
-    char* buffer;
-    long filelen;
+    FILE* fileptr = NULL;
+    char* buffer = NULL;
     char* encap = NULL;
+    long filelen;
+    int encap_size_val = 0;
+
+    fileptr = fopen(filename, "rb");                   // Open the file in binary mode
+    fseek(fileptr, 0, SEEK_END);                       // Jump to the end of the file
+    filelen = ftell(fileptr);                          // Get the current byte offset in the file
+    rewind(fileptr);                                   // Jump back to the beginning of the file
+
+    buffer = (char *)malloc((filelen+1) * sizeof(char));
+    fread(buffer, filelen, 1, fileptr);
+
+    encap = encapsulate_des_compatable_size(buffer, filelen + 1);
+    encap_size_val = encap_size(buffer, filelen + 1);
+
+    fclose(fileptr); // Close the file
+    fileptr = NULL;
+    free(buffer);
+    buffer = NULL;
+
+    return encap;
+}
+
+void encrypt_file_byte_write(char* filename, char* buffer, int size){
+    FILE* fileptr = NULL;
+    int filelen = size;
+    int decap_size_val = 0;
+    char* decap = NULL;
+
+    decap = char_array_init(filelen + 1);
+    char_array_shallow_copy(decap, buffer, 0, ENCAP_HEADER_SIZE, filelen);
+    decap[filelen] = '\0';
+
+    fileptr = fopen(filename, "wb");                                                                           
+    fwrite(decap, 1, filelen + 1, fileptr);
+
+    fclose(fileptr); // Close the file
+    fileptr = NULL;
+    free(decap);
+    decap = NULL;
+}
+
+char* decrypt_file_byte_read(char* filename){
+    FILE* fileptr = NULL;
+    char* buffer = NULL;
+    char* encap = NULL;
+    long filelen;
     int encap_size_val = 0;
 
     fileptr = fopen(filename, "rb");                   // Open the file in binary mode
@@ -20,35 +80,29 @@ char* encrypt_file_byte_read(char* filename){
     encap_size_val = encap_size(buffer, filelen + 1);
 
     fclose(fileptr); // Close the file
+    fileptr = NULL;
     free(buffer);
     buffer = NULL;
 
     return encap;
 }
 
-void encrypt_file_byte_write(char* filename, char* buffer){
-    FILE* fileptr;
-    char* buffer;
-    long filelen;
+void decrypt_file_byte_write(char* filename, char* buffer){
+    FILE* fileptr = NULL;
     char* decap = NULL;
+    long filelen;
     int decap_size_val = 0;
 
-    char* locked_name = char_array_init(sizeof(filename) / sizeof(char));
+    filelen = decap_size(buffer);
+    decap = (char*)malloc(sizeof(char) * filelen);
+    char_array_shallow_copy(decap, buffer, 0, ENCAP_HEADER_SIZE, filelen);
 
-    fileptr = fopen(filename, "wb");                   // Open the file in binary mode
-    fseek(fileptr, 0, SEEK_END);                            // Jump to the end of the file
-    filelen = ftell(fileptr);                               // Get the current byte offset in the file
-    rewind(fileptr);                                        // Jump back to the beginning of the file
-
-    buffer = (char *)malloc((filelen+1) * sizeof(char));
-    fread(buffer, filelen, 1, fileptr);
-
-    encap = encapsulate_des_compatable_size(buffer, filelen + 1);
-    encap_size_val = encap_size(buffer, filelen + 1);
+    fileptr = fopen(filename, "wb");                                          
+    rewind(fileptr);                                   
+    fwrite(decap, filelen, 1, fileptr);
 
     fclose(fileptr); // Close the file
-    free(buffer);
-    buffer = NULL;
-
-    return encap;
+    fileptr = NULL;
+    free(decap);
+    decap = NULL;
 }
--- fileio.h
+++ fileio.h
@@ -4,7 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int filesize(char*);
+char* encrypt_file_byte_read(char*);
+void encrypt_file_byte_write(char*, char*, int);
+char* decrypt_file_byte_read(char*);
+void decrypt_file_byte_write(char*, char*);
 
-
-
-#endif FILEIO
+#endif
